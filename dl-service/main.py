479e4dcf0a68b31f83f160724b2a3d3dc8bdb6ef
sudhirsingh <crepantherx@gmail.com>
2025-11-23 17:38:04 +0530

    a

--- dl-service/main.py
+++ dl-service/main.py
@@ -69,4 +69,6 @@ def predict(request: ForecastRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8003)
+    import os
+    port = int(os.environ.get("PORT", 8003))
+    uvicorn.run(app, host="0.0.0.0", port=port)
